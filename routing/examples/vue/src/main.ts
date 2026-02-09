0f0251cad0533fa1c6e87651b2db1db1c956a939
cosmic.gao <cosmic.gao@outlook.com>
2026-02-09 11:27:54 +0800

    feat: 初始化 Vue 应用并配置路由

--- routing/examples/vue/src/main.ts
+++ routing/examples/vue/src/main.ts
@@ -3,8 +3,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import routes from 'virtual:routing/routes'
 import App from './App.vue'
 
-console.log(routes)
-
 const router = createRouter({
     history: createWebHistory(),
     routes,
